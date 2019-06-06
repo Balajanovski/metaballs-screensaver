ce77cd518efc550c0851d48b0a65d40410483556
Balajanovski <jbalajan@gmail.com>
2019-06-06 18:53:11 +1000

    - Added Russian Locale

--- Metaballs/ConfigurationWindow.xaml.vb
+++ Metaballs/ConfigurationWindow.xaml.vb
@@ -1,15 +1,25 @@
 ﻿Imports Microsoft.Win32
 Imports System.Windows.Forms
+Imports System.Globalization
 
 Public Class ConfigurationWindow
 
     Private savedSettings As Boolean = False
 
-    Public Sub New()
+    Private initialising As Boolean = True
+
+    Private languageChanged As Boolean = False
+
+    Public Sub New(Optional selectedLanguage As Integer = 0)
         ' This call is required by the designer.
         InitializeComponent()
 
         savedSettings = False
+
+        languageBox.SelectedIndex = selectedLanguage
+        UpdateText()
+
+        initialising = False
     End Sub
 
     Private Sub SaveSettings()
@@ -27,10 +37,9 @@ Public Class ConfigurationWindow
     End Sub
 
     Private Sub Window_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs)
-        If Not savedSettings Then
-            Dim result As DialogResult = MessageBox.Show("You are about to exit without saving" _
-                & vbCrLf & "Are you sure you want to proceed",
-                                                      "Exit Without Saving?",
+        If Not savedSettings And Not languageChanged Then
+            Dim result As DialogResult = MessageBox.Show(My.Resources.Locale.ExitWithoutSavingMsg,
+                                                      My.Resources.Locale.ExitWithoutSavingTitle,
                                                       MessageBoxButtons.YesNo,
                                                       MessageBoxIcon.Question,
                                                       MessageBoxDefaultButton.Button1)
@@ -50,24 +59,49 @@ Public Class ConfigurationWindow
     Private Sub SpeedSlider_ValueChanged(sender As Object, e As Windows.RoutedPropertyChangedEventArgs(Of Double)) Handles speedSlider.ValueChanged
         savedSettings = False
 
-        speedLabel.Content = "Speed (Value: " & String.Format("{0:0.00}", speedSlider.Value) & "):"
+        speedLabel.Text = My.Resources.Locale.Speed & String.Format("{0:0.00}", speedSlider.Value) & "):"
     End Sub
 
     Private Sub DropletNumberSlider_ValueChanged(sender As Object, e As Windows.RoutedPropertyChangedEventArgs(Of Double)) Handles dropletNumberSlider.ValueChanged
         savedSettings = False
 
-        dropletNumberLabel.Content = "Droplet Number (Value: " & String.Format("{0:0}", dropletNumberSlider.Value) & "):"
+        dropletNumberLabel.Text = My.Resources.Locale.DropletNumber & String.Format("{0:0}", dropletNumberSlider.Value) & "):"
     End Sub
 
     Private Sub WaveheightSlider_ValueChanged(sender As Object, e As Windows.RoutedPropertyChangedEventArgs(Of Double)) Handles waveheightSlider.ValueChanged
         savedSettings = False
 
-        waveheightLabel.Content = "Waveheight (Value: " & String.Format("{0:0.00}", waveheightSlider.Value) & "):"
+        waveheightLabel.Text = My.Resources.Locale.Waveheight & String.Format("{0:0.00}", waveheightSlider.Value) & "):"
     End Sub
 
     Private Sub ResolutionSlider_ValueChanged(sender As Object, e As Windows.RoutedPropertyChangedEventArgs(Of Double)) Handles resolutionSlider.ValueChanged
         savedSettings = False
 
-        resolutionLabel.Content = "Resolution Ratio (Value: 1:" & String.Format("{0:0}", resolutionSlider.Value) & "):"
+        resolutionLabel.Text = My.Resources.Locale.ResolutionRatio & String.Format("{0:0}", resolutionSlider.Value) & "):"
+    End Sub
+
+    Private Sub UpdateText()
+        speedLabel.Text = My.Resources.Locale.Speed & String.Format("{0:0.00}", speedSlider.Value) & "):"
+        dropletNumberLabel.Text = My.Resources.Locale.DropletNumber & String.Format("{0:0}", dropletNumberSlider.Value) & "):"
+        waveheightLabel.Text = My.Resources.Locale.Waveheight & String.Format("{0:0.00}", waveheightSlider.Value) & "):"
+        resolutionLabel.Text = My.Resources.Locale.ResolutionRatio & String.Format("{0:0}", resolutionSlider.Value) & "):"
+    End Sub
+
+    Private Sub LanguageBox_SelectionChanged(sender As Object, e As Windows.Controls.SelectionChangedEventArgs) Handles languageBox.SelectionChanged
+        Dim stackPanel = DirectCast(DirectCast(languageBox.SelectedItem, Windows.Controls.ComboBoxItem).Content,
+            Windows.Controls.StackPanel)
+
+        Dim lang As String
+        For Each child In stackPanel.Children.OfType(Of Windows.Controls.TextBlock)
+            lang = child.Text
+        Next
+
+        My.Resources.Locale.Culture = New CultureInfo(lang)
+
+        ' Prevent infinite loop
+        If Not initialising Then
+            languageChanged = True
+            Program.ReloadWindow(languageBox.SelectedIndex) ' Reload window upon changing language
+        End If
     End Sub
 End Class
